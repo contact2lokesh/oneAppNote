17dae92870103b78cbb7a08d5d5d7ad557b43fe1
contact2lokesh <contacttolokesh22@gmail.com>
2022-03-10 12:27:01 +0530

    deploying to heroku

--- oneAppNote/server/server.js
+++ oneAppNote/server/server.js
@@ -5,17 +5,34 @@ import userRoutes from "./routes/userRoutes.js";
 import noteRoute from "./routes/noteRoute.js";
 import {notFound, errorHandler} from "./middlewares/errormidleware.js";
 import cors from 'cors';
-
+import path from 'path';
 const app = express();
 dotEnv.config();
 connectDB();
 app.use(express.json());  // for json data
+
 app.use(cors({
   origin: "http://localhost:3000",
 }));
  
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoute);
+ 
+// ------------deploying---------
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// ---------------deploying--------
 
 //
 // Error Handling middleware
